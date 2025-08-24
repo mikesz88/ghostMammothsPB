562de5850b29c11e38c9fb11ff1a19d682b5fe47
Iconians <claytoncripe@gmail.com>
2025-08-23 19:42:58 -0700

    added the pages

---
+++ src/app/Membership/page.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "../componets/navbar/navbar";
+import styles from "./membership.module.css";
+
+export default function Membership() {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>Membership page</h1>
+      </div>
+    </>
+  );
+}
