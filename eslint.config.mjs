1cdf767807bfdb4cddede6784cdf84c6ee5b0c2f
Iconians <claytoncripe@gmail.com>
2026-04-20 11:45:15 -0700

    cleaned eslint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,3 @@
-// import nextConfig from "eslint-config-next/core-web-vitals";
-// import jsxA11y from "eslint-plugin-jsx-a11y";
-
-// const eslintConfig = [
-//   {
-//     ignores: [
-//       "node_modules/**",
-//       ".next/**",
-//       "out/**",
-//       "build/**",
-//       "next-env.d.ts",
-//     ],
-//   },
-//   ...nextConfig,
-//   // Strict a11y: catch things devs might forget (labels, roles, no nested interactive, etc.)
-//   { rules: jsxA11y.configs.strict.rules },
-//   {
-//     ignores: [
-//       "node_modules/**",
-//       ".next/**",
-//       "out/**",
-//       "build/**",
-//       "next-env.d.ts",
-//     ],
-//   },
-// ];
-
-// export default eslintConfig;
-
 import nextConfig from "eslint-config-next/core-web-vitals";
 import importPlugin from "eslint-plugin-import";
 import unusedImports from "eslint-plugin-unused-imports";
